db5350ebecf11060a12a6e36ca51177a2bd6afcd
GPiovesan <bieldeluna@gmail.com>
2023-01-31 23:25:03 -0300

    feature(measure.rb): Gerado scaffold para measures e vinculação das tabelas

--- db/migrate/20230201001639_create_products.rb
+++ db/migrate/20230201001639_create_products.rb
@@ -4,8 +4,10 @@ class CreateProducts < ActiveRecord::Migration[7.0]
       t.string :description
       t.decimal :price
       t.string :image_path
+      t.integer :in_stock
+      t.boolean :featured
       t.belongs_to :categories
-
+      t.belongs_to :measures
       t.timestamps
     end
   end
