67a4a8ef590d5711bdf39ba5808156a8d6516f62
GPiovesan <bieldeluna@gmail.com>
2023-02-02 00:57:19 -0300

    feat(category_spec.rb): Criado CRUD de categorias com TDD

--- spec/features/category_spec.rb
+++ spec/features/category_spec.rb
@@ -6,10 +6,35 @@ feature "Categories", type: :feature do
     expect(page).to have_link('Categorias')
   end
 
-  scenario 'Verifica link Nova Categoria' do
+  scenario 'Link Nova Categoria' do
     visit(root_path)
     click_on('Categorias')
     expect(page).to have_content('Listando Categorias')
     expect(page).to have_link('Nova Categoria')
+  end
+
+  scenario 'Formulário de Nova Categoria' do
+    visit(categories_path)
+    click_link('Nova Categoria')
+    expect(page).to have_button('Criar Categoria')
+  end
+
+  scenario 'Cadastra uma categoria válida' do
+    visit(new_category_path)
+    category_name = Faker::Commerce.department
+    
+    fill_in('Categoria', with: category_name)
+    click_on('Criar Categoria')
+
+    expect(page).to have_content('Categoria cadastrada com sucesso')
+    expect(Category.last.category).to eq(category_name)
+  end
+
+  scenario 'Cadastra um Produto Inválido' do
+    visit(new_category_path)
+    
+    click_on('Criar Categoria')
+
+    expect(page).to have_content('Categoria não pode ficar em branco')
   end
 end
